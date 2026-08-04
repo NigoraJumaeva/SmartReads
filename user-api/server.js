3fd9006b0e895b2645e80e144cefaeb5ad1b6032
Nigora Jumaeva <njumaeva@myseneca.ca>
2026-08-04 02:30:43 -0400

    last changes

--- user-api/server.js
+++ user-api/server.js
@@ -21,7 +21,21 @@ const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
 
-app.use(cors());
+app.use(
+    cors({
+        origin:[
+            "http://localhost:3000",
+            "https://smart-reads-saam.vercel.app"
+        ],
+        methods:[
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE"
+        ],
+        credentials:true
+    })
+);
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
