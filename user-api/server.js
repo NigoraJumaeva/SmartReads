dcbd0d9a1f3cd0f9633eaf608f618b7f86294fcc
Nigora Jumaeva <njumaeva@myseneca.ca>
2026-08-04 02:48:11 -0400

    cors changed again

--- user-api/server.js
+++ user-api/server.js
@@ -21,7 +21,10 @@ const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
     next();
